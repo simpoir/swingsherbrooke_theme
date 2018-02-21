39fbd74d75bb2cfd3de904d9a9002f621d5604b2
Simon Poirier <simon.poirier@canonical.com>
2018-02-21 14:07:04 -0500

    fork theme name

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -8,33 +8,33 @@
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
- * @package _s
+ * @package swingsherbrooke
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses _s_header_style()
+ * @uses swingsherbrooke_header_style()
  */
-function _s_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( '_s_custom_header_args', array(
+function swingsherbrooke_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'swingsherbrooke_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => '_s_header_style',
+		'wp-head-callback'       => 'swingsherbrooke_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', 'swingsherbrooke_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( 'swingsherbrooke_header_style' ) ) :
 	/**
 	 * Styles the header image and text displayed on the blog.
 	 *
-	 * @see _s_custom_header_setup().
+	 * @see swingsherbrooke_custom_header_setup().
 	 */
-	function _s_header_style() {
+	function swingsherbrooke_header_style() {
 		$header_text_color = get_header_textcolor();
 
 		/*
