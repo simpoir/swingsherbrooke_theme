fb0ac77ff66dff9e0742c9843d1d943b7fd773e9
Simon Poirier <simon.poirier@canonical.com>
2018-02-21 22:03:46 -0500

    cleanup + packaging

--- footer.php
+++ footer.php
@@ -15,14 +15,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'swingsherbrooke' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'swingsherbrooke' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
 			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'swingsherbrooke' ), 'swingsherbrooke', '<a href="https://automattic.com/">Automattic</a>' );
+				printf( esc_html( '© %s Swing Sherbrooke' ), date('Y') );
 			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
