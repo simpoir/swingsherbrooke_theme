54a2d439c25d019bd473c9bc1cf5205a8a31a62c
Simon Poirier <simpoir@gmail.com>
2018-04-05 20:06:41 -0400

    remove comment and center default template

--- page.php
+++ page.php
@@ -17,22 +17,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+			<div id="central-container" class="container">
+
 			<?php
 			while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
 			endwhile; // End of the loop.
 			?>
+
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
